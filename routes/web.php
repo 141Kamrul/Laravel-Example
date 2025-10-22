40c0c68fe1a55308a556096d4be0b4f23bd85370
Md. Kamrul Hasan <bsse1418@iit.du.ac.bd>
2025-10-22 11:32:38 +0600

    step 8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,28 +26,38 @@ Route::get('/foo', function() {
     return ['foo'=>'bar'];
 });
 
+// Route::get('/jobs', function () {
+//     return view('jobs', [
+//         'jobs' => [
+//             ['id' => 1, 'title' => 'Director', 'salary' => 50000],
+//             ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
+//             ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
+//         ],
+//     ]);
+// });
+
+// Route::get('/jobs/{id}', function ($id) {
+//     $jobs=[
+//         ['id' => 1, 'title' => 'Director', 'salary' => 50000],
+//         ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
+//         ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
+//     ];
+
+//     $job = collect($jobs)->first(fn ($job) => $job['id'] == $id);
+
+//     if(!$job){
+//         abort(404);
+//     }
+
+//     return view('job', ['job' => $job]);
+// });
+
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-        ],
-    ]);
+    $jobs=Job::all();
+    return view('jobs', ['jobs'=>$jobs]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs=[
-        ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-        ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-        ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-    ];
-
-    $job = collect($jobs)->first(fn ($job) => $job['id'] == $id);
-
-    if(!$job){
-        abort(404);
-    }
-
-    return view('job', ['job' => $job]);
+Route::get('jobs/{id}', function($id){
+    $job=Job::find($id);
+    return view('job',['job'=>$job]);
 });
